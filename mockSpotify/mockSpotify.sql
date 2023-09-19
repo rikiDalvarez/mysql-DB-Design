3371baf2a039f040e67685056d3d9579dd5b5903
Ricardo <ric.dalvarez@hotmail.com>
2023-09-19 11:03:13 +0200

    erase comments

--- mockSpotify/mockSpotify.sql
+++ mockSpotify/mockSpotify.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.8.4
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 29-06-2023 a las 10:50:46
--- Versión del servidor: 10.1.37-MariaDB
--- Versión de PHP: 5.6.39
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -13,13 +6,8 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 DROP DATABASE IF EXISTS mockSpotify;
-CREATE DATABASE mockSoptify;
+CREATE DATABASE mockSpotify;
 USE mockSpotify;
 --
 -- Base de datos: `mockSpotify`
--- pizzaPlanet/pizzaPlanet.sql
+++ pizzaPlanet/pizzaPlanet.sql
@@ -1,22 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.8.4
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 27-06-2023 a las 17:21:05
--- Versión del servidor: 10.1.37-MariaDB
--- Versión de PHP: 5.6.39
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 DROP DATABASE IF EXISTS pizzaPlanet;
 CREATE DATABASE pizzaPlanet;
